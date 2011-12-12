461063b0b0f5f615e88e547badb0cc088426b399
steven <steven@urethrafranklin.org>
2011-12-11 23:33:19 -0500

    separating js on index.html

---
+++ static/javascript/audio.js
@@ -0,0 +1,95 @@
+//binds audio files on the index.html page to their own individual buttons
+
+$(document).ready(function() {
+      
+    $("#jquery_jplayer_1").jPlayer({
+      ready: function () {
+          $(this).jPlayer("setMedia", {
+            mp3: "media/demo.mp3"
+          });
+        },
+        ended: function (event) {
+          $(this);
+        },
+        supplied: "mp3"
+      }).bind($.jPlayer.event.play, function() { // Using a jPlayer event to avoid both jPlayers playing together.
+      $(this).jPlayer("pauseOthers");
+    });
+
+    $("#jquery_jplayer_2").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "media/English_Commercial Demo.mp3"
+        });
+      },
+      ended: function (event) {
+        $(this);
+      },
+      cssSelectorAncestor:"#jp_interface_2",
+      supplied: "mp3"
+      }).bind($.jPlayer.event.play, function() { 
+      $(this).jPlayer("pauseOthers");
+    });
+    
+    $("#jquery_jplayer_3").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "media/English_Narration_Demo.mp3"
+        });
+      },
+      ended: function (event) {
+        $(this);
+      },
+      cssSelectorAncestor:"#jp_interface_3",
+      supplied: "mp3"
+      }).bind($.jPlayer.event.play, function() { 
+      $(this).jPlayer("pauseOthers");
+    });
+
+    $("#jquery_jplayer_4").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "media/EnglishIVR.mp3"
+        });
+      },
+      ended: function (event) {
+        $(this);
+      },
+      cssSelectorAncestor:"#jp_interface_4",
+      supplied: "mp3"
+      }).bind($.jPlayer.event.play, function() { 
+      $(this).jPlayer("pauseOthers");
+    });
+
+    $("#jquery_jplayer_5").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "media/FrenchNarration.mp3"
+        });
+      },
+      ended: function (event) {
+        $(this);
+      },
+      cssSelectorAncestor:"#jp_interface_5",
+      supplied: "mp3"
+      }).bind($.jPlayer.event.play, function() { 
+      $(this).jPlayer("pauseOthers");
+    });
+
+
+    $("#jquery_jplayer_6").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "media/French_IVR.mp3"
+        });
+      },
+      ended: function (event) {
+        $(this);
+      },
+      cssSelectorAncestor:"#jp_interface_6",
+      supplied: "mp3"
+      }).bind($.jPlayer.event.play, function() { 
+      $(this).jPlayer("pauseOthers");
+    });
+    
+});
